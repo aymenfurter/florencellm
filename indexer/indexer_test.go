fef759bd4fdb1b812ee50a67ac688126b3108cac
Aymen Furter <aymen.furter@gmail.com>
2023-04-20 21:07:18 +0200

    Update indexer_test.go

--- indexer/indexer_test.go
+++ indexer/indexer_test.go
@@ -9,16 +9,13 @@ import (
 )
 
 func TestProcessRepository(t *testing.T) {
-	// until bd674acdbc7907ca30352081bc7137511b2e79c7
 	ctx := context.Background()
 
 	repo := Repository{
-		//URL: "https://github.com/MicrosoftDocs/architecture-center.git",
 		URL: "https://github.com/MicrosoftDocs/azure-docs.git",
-		//URL: "https://github.com/MicrosoftDocs/azure-docs-cli.git",
 	}
 
-	err := processRepository(ctx, repo, "e531c61632f0d4382c7d53213b7a8dfc36351cf9")
+	err := processRepository(ctx, repo, "9b7fd5df4b4491048261010f34db86402f320a41")
 	//err := processRepository(ctx, repo, "")
 	require.NoError(t, err)
 }
