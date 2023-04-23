9a7e23f3964e7cb727a4ae073bdc5016cf6ba7c8
Aymen Furter <aymen.furter@gmail.com>
2023-04-23 17:25:21 +0200

    feat: Improve WebUI

--- webui/src/app/app-routing.module.ts
+++ webui/src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ChatComponent } from './chat/chat.component';
 
 const routes: Routes = [
-  { path: 'chat', component: ChatComponent }, // Add this line
-  { path: '', redirectTo: '/chat', pathMatch: 'full' } // Update this line to redirect to the chat component by default
+  { path: '', component: ChatComponent }, 
 ];
 
 @NgModule({
